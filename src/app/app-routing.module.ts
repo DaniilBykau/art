80ee26dcd6fededc0872d9284e510df513c55e16
Daniil <68930860+DaniilBykau@users.noreply.github.com>
2023-01-22 01:06:26 +0100

    components + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BaseComponent} from "./components/base/base.component";
+import {ImagesComponent} from "./components/images/images.component";
+import {ImageDetailsComponent} from "./components/image-details/image-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BaseComponent},
+  {path: 'images', component: ImagesComponent},
+  {path: 'image/:id', component: ImageDetailsComponent},
+  {path: '**', redirectTo: "", component: BaseComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/ui/header/header.component';
+import { FooterComponent } from './components/ui/footer/footer.component';
+import { BaseComponent } from './components/base/base.component';
+import { ImagesComponent } from './components/images/images.component';
+import { ImageDetailsComponent } from './components/image-details/image-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BaseComponent,
+    ImagesComponent,
+    ImageDetailsComponent
   ],
   imports: [
     BrowserModule,
